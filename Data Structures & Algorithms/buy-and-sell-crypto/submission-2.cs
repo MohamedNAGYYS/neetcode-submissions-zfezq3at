6551bf468498cba08375e89ee4a6df937e340a70
M.Nagy <161846669+MohamedNAGYYS@users.noreply.github.com>
2026-04-08 03:13:08 +0200

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        if (prices == null || prices.Length == 0){return 0;}
+
+            int minPrice = prices[0];
+            int maxProfit = 0;
+
+
+            foreach(int price in prices)
+            {
+                minPrice = Math.Min(minPrice, price);
+                maxProfit = Math.Max(maxProfit,price-minPrice);
+            }
+            return maxProfit;
+
+    }
+}
